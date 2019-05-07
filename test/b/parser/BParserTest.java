65fe440ffaf8089e2753c15000262a98337897e4
stratosphr <stratosphr@gmail.com>
2019-05-07 21:08:45 +0200

    More tests

--- test/b/parser/BParserTest.java
+++ test/b/parser/BParserTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by gvoiron on 04/05/19.
@@ -19,16 +20,21 @@ class BParserTest {
 
     @Test
     void setInputFile_fileFound() {
-        assertAll(
-                () -> assertDoesNotThrow(() -> BParser.setInputFile("res/machine.mch")),
-                () -> assertDoesNotThrow(() -> BParser.setInputFile("res/arithExpr.mch")),
-                () -> assertDoesNotThrow(() -> BParser.setInputFile("res/singleVarExpr.mch"))
-        );
+        assertDoesNotThrow(() -> BParser.setInputFile("res/singleVarExpr.mch"));
+        assertDoesNotThrow(() -> BParser.setInputFile("res/arithExpr.mch"));
+        assertDoesNotThrow(() -> BParser.setInputFile("res/boolExpr.mch"));
+        assertDoesNotThrow(() -> BParser.setInputFile("res/machine.mch"));
     }
 
     @Test
-    void parseExpr_noSyntaxError_arithexpr() {
+    void parseExpr_noSyntaxError_arithExpr() {
         assertDoesNotThrow(() -> BParser.setInputFile("res/arithExpr.mch"));
+        assertDoesNotThrow(BParser::parseExpr0);
+    }
+
+    @Test
+    void parseExpr_noSyntaxError_boolExpr() {
+        assertDoesNotThrow(() -> BParser.setInputFile("res/boolExpr.mch"));
         assertDoesNotThrow(BParser::parseExpr0);
     }
 
